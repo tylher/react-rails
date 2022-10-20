532612005c4f4a2f198dac1986a6fc8c002fc65e
unknown <tylher123@gmail.com>
2022-10-20 07:41:09 +0100

    install react dependencies

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,5 @@
-class API::V1::Greetings < ApiController
-    def index
+class API::V1::GreetingsController < APIController
+    def show
         @randomGreetings = Greeting.order("RANDOM()").first
         respond_to do |format|
             format.json { render :json => @randomGreetings}
